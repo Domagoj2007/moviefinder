b07cc9bfc7768354c629182eb163a1407feee822
kingfran1907 <fran.babic1907@gmail.com>
2017-07-05 19:27:24 +0200

    new models

--- moviefinder/models.py
+++ moviefinder/models.py
@@ -6,7 +6,13 @@ class Movie(models.Model):
 	description = models.CharField(max_length=3000)
 	votes = models.IntegerField(default=0)
 
+	def __str__(self):
+		return self.title
+
 class Event(models.Model):
 	location = models.CharField(max_length=100)
 	vrijeme = models.DateTimeField('Date Published')
 	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.location
