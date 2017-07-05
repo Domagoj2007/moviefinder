1c5f80bccce8cda06eec36e403c1ed4a89874ee9
kingfran1907 <fran.babic1907@gmail.com>
2017-07-05 19:18:24 +0200

    dodao modele

--- moviefinder/models.py
+++ moviefinder/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+	title = models.CharField(max_length=50)
+	description = models.CharField(max_length=3000)
+	votes = models.IntegerField(default=0)
+
+class Event(models.Model):
+	location = models.CharField(max_length=100)
+	vrijeme = models.DateTimeField('Date Published')
+	movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
